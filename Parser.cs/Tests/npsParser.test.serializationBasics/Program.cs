ce73ac715d1a7e7dba3d367511b36f1711d3f528
noslopforever <noslopdev@hotmail.com>
2023-02-03 15:01:19 +0800

    Add serialization test-suits.

---
+++ Parser.cs/Tests/npsParser.test.serializationBasics/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using nf.protoscript;
+using nf.protoscript.syntaxtree;
+using System.Runtime.InteropServices;
+using nf.protoscript.Serialization;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+namespace nf.protoscript.test
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int procID = Process.GetCurrentProcess().Id;
+
+            ProjectInfo testProj = TestCases.BasicLanguage();
+
+            {
+                // Serialization: serialize testProj to an InfoSerializationData.
+                var serialData = InfoSerializer.Serialize(testProj);
+
+                // Test deserialization from data immediately
+                {
+                    try
+                    {
+                        ProjectInfo deserialProj = InfoSerializer.Deserialize(null, serialData) as ProjectInfo;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("[ERROR] Immediately deserialize failed.");
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                // test formatter: XML
+                {
+                    try
+                    {
+                        XmlSerializer xmlSerial = new XmlSerializer(typeof(InfoSerializationData));
+                        TextWriter writer = new StringWriter();
+                        xmlSerial.Serialize(writer, serialData);
+                        Console.WriteLine(writer.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("[ERROR] Xml writer failed.");
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+
+                // test formatter: Json
+                {
+                    try
+                    {
+                        string jsonStr = JsonSerializer.Serialize(serialData);
+                        Console.WriteLine(jsonStr);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("[ERROR] Json writer failed.");
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
