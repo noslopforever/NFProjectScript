07128b4a6186c7c486131c42ae9fcb37453b8825
noslopdev <noslopdev@hotmail.com>
2023-03-30 20:54:22 +0800

    Add Unexpected token checks.
    * RT.

--- Parser.cs/npsParser.parser/SyntaxV1/SyntaxAnalysis/ASTParser_StatementDefFunction.cs
+++ Parser.cs/npsParser.parser/SyntaxV1/SyntaxAnalysis/ASTParser_StatementDefFunction.cs
@@ -78,6 +78,14 @@ namespace nf.protoscript.parser.syntax1.analysis
                 comments => result._Internal_AddComments(comments)
                 );
 
+            // if not end, there is an unexpected token
+            if (!InTokenList.IsEnd)
+            {
+                // TODO log error
+                throw new NotImplementedException();
+                return null;
+            }
+
             return result;
         }
     }
