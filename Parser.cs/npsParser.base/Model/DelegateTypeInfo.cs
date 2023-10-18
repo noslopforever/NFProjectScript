fe7cfbd2c2bd6947d5433691401dca0da7f2b9b3
noslopdev <noslopdev@hotmail.com>
2023-10-18 12:54:03 +0800

    Customize DelegateTypeInfo.ToString
    + DelegateTypeInfo: Show delegate type in "name(params)[: return]" form.

--- Parser.cs/npsParser.base/Model/DelegateTypeInfo.cs
+++ Parser.cs/npsParser.base/Model/DelegateTypeInfo.cs
@@ -17,6 +17,32 @@
         /// </summary>
         public TypeInfo ReturnType { get; private set; }
 
+        public override string ToString()
+        {
+            string typeName = base.ToString();
+
+            // Gather params
+            string paramsStr = "";
+            ForeachSubInfoByHeader<ElementInfo>("param", param =>
+            {
+                paramsStr += $"{param.Name}, ";
+            });
+            // Try removing the last ', '
+            if (paramsStr.Length > 2)
+            {
+                paramsStr = paramsStr.Remove(paramsStr.Length - 2);
+            }
+
+            // Gather return string.
+            string retStr = "";
+            if (ReturnType != null)
+            {
+                retStr = $": {ReturnType}";
+            }
+
+            return $"{typeName}({paramsStr}){retStr}";
+        }
+
     }
 
 
